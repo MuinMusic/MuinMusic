58bf1d91dcb83ed4c20ee9ae5f548009489d377d
sdongpil <pildong94@naver.com>
2023-05-26 22:59:57 +0900

    second commit

---
+++ src/main/java/com/mucompany/muinmusic/MuinMusicApplication.java
@@ -0,0 +1,13 @@
+package com.mucompany.muinmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuinMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuinMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mucompany/muinmusic/MuinMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mucompany.muinmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuinMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
