21459359ea80a3fba088b28dc2ad81b41a71ea78
sdongpil <pildong94@naver.com>
2023-05-28 21:04:31 +0900

    refactor : Article 필드 추가

--- src/main/java/com/mucompany/muinmusic/article/Article.java
+++ src/main/java/com/mucompany/muinmusic/article/Article.java
@@ -4,9 +4,11 @@ import com.mucompany.muinmusic.domain.Genre;
 import com.mucompany.muinmusic.domain.Part;
 import com.mucompany.muinmusic.domain.Skill;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.util.Assert;
 
 @Entity
 @Getter
@@ -17,6 +19,9 @@ public class Article {
     @GeneratedValue
     @Column(name = "article_id")
     private Long id;
+
+    @NotNull
+    private Long userId;
 
     @NotNull
     private String title;
@@ -38,14 +43,26 @@ public class Article {
     @Enumerated(EnumType.STRING)
     private Genre genre;
 
-    public Article(Long id, String title, String career, int price, Part part, Skill skill, Genre genre) {
+    private int views;
+
+    @Builder
+    public Article(Long id, Long userId, String title, String career, int price, Part part, Skill skill, Genre genre,int views) {
+        Assert.notNull(userId, "userId 필수값입니다");
+        Assert.notNull(title, "title 필수값입니다");
+        Assert.notNull(price, "price 필수값입니다");
+        Assert.notNull(part, "part 필수값입니다");
+        Assert.notNull(skill, "skill 필수값입니다");
+        Assert.notNull(genre, "genre 필수값입니다");
+
         this.id = id;
+        this.userId = userId;
         this.title = title;
         this.career = career;
         this.price = price;
         this.part = part;
         this.skill = skill;
         this.genre = genre;
+        this.views = views;
     }
 
 }
