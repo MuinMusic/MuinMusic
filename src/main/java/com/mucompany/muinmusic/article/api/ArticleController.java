648662def6cb6eac18852a0d14d06bb491bf241d
sdongpil <pildong94@naver.com>
2023-05-28 21:51:32 +0900

    feat : ArticleController - 게시글 삭제 작성

--- src/main/java/com/mucompany/muinmusic/article/api/ArticleController.java
+++ src/main/java/com/mucompany/muinmusic/article/api/ArticleController.java
@@ -3,12 +3,11 @@ package com.mucompany.muinmusic.article.api;
 import com.mucompany.muinmusic.article.Article;
 import com.mucompany.muinmusic.article.api.dto.ArticleCreateDto;
 import com.mucompany.muinmusic.article.app.ArticleService;
+import com.mucompany.muinmusic.exception.ArticleNotFoundException;
 import com.mucompany.muinmusic.exception.ArticleValidityCheckException;
 import com.mucompany.muinmusic.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +18,11 @@ public class ArticleController {
     @PostMapping(value = "/article")
     public Article create(@RequestBody ArticleCreateDto articleCreateDto) throws UserNotFoundException, ArticleValidityCheckException {
         return articleService.createArticle(articleCreateDto, getCurrentUserId());
+    }
+
+    @DeleteMapping(value = "/article/{articleId}")
+    public void delete(@PathVariable Long articleId) throws ArticleNotFoundException {
+        articleService.delete(articleId);
     }
 
     private Long getCurrentUserId() {
