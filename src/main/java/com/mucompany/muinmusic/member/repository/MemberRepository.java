9c9813270aee2c2ba2fbd35e23c7ea58ef0c5d75
sdongpil <pildong94@naver.com>
2023-05-29 04:31:41 +0900

    refactor : 불필요한 클래스 삭제

--- src/main/java/com/mucompany/muinmusic/member/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mucompany.muinmusic.member.repository;
-
-import com.mucompany.muinmusic.member.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-
-}
