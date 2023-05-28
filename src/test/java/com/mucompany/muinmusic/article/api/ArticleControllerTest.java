3cb066f1313cea9435badd712cf7159cf9f88099
sdongpil <pildong94@naver.com>
2023-05-28 21:52:00 +0900

    test : Article - 게시글 삭제 테스트 작성

--- src/test/java/com/mucompany/muinmusic/article/api/ArticleControllerTest.java
+++ src/test/java/com/mucompany/muinmusic/article/api/ArticleControllerTest.java
@@ -1,7 +1,9 @@
 package com.mucompany.muinmusic.article.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mucompany.muinmusic.article.Article;
 import com.mucompany.muinmusic.article.api.dto.ArticleCreateDto;
+import com.mucompany.muinmusic.article.repository.ArticleRepository;
 import com.mucompany.muinmusic.domain.Genre;
 import com.mucompany.muinmusic.domain.Part;
 import com.mucompany.muinmusic.domain.Skill;
@@ -13,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,6 +28,8 @@ public class ArticleControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @DisplayName(value = "articleCreateDto 유효하면 저장성공")
     @Test
@@ -49,5 +54,31 @@ public class ArticleControllerTest {
                 .andExpect(jsonPath("$..career").value(articleCreateDto.getCareer()))
                 .andExpect(jsonPath("$..part").value(part))
                 .andExpect(jsonPath("$..genre").value(articleCreateDto.getGenre().toString()));
+    }
+
+    @DisplayName(value = "articleId 유효하면 삭제 성공")
+    @Test
+    void t2() throws Exception {
+
+        createArticle();
+
+        Long articleId = 1L;
+
+        mockMvc.perform(delete("/article/{articleId}", articleId).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    private void createArticle() {
+        Article article = Article.builder()
+                .title("안녕하세요")
+                .userId(1L)
+                .career("누구누구 세션 , 교회 반주 10년")
+                .price(30000)
+                .part(Part.LIVE_SESSION)
+                .skill(Skill.GUITAR)
+                .genre(Genre.POP)
+                .build();
+
+        articleRepository.save(article);
     }
 }
--- src/test/java/com/mucompany/muinmusic/article/app/ArticleServiceTest.java
+++ src/test/java/com/mucompany/muinmusic/article/app/ArticleServiceTest.java
@@ -2,9 +2,11 @@ package com.mucompany.muinmusic.article.app;
 
 import com.mucompany.muinmusic.article.Article;
 import com.mucompany.muinmusic.article.api.dto.ArticleCreateDto;
+import com.mucompany.muinmusic.article.repository.ArticleRepository;
 import com.mucompany.muinmusic.domain.Genre;
 import com.mucompany.muinmusic.domain.Part;
 import com.mucompany.muinmusic.domain.Skill;
+import com.mucompany.muinmusic.exception.ArticleNotFoundException;
 import com.mucompany.muinmusic.exception.ArticleValidityCheckException;
 import com.mucompany.muinmusic.exception.UserNotFoundException;
 import org.junit.jupiter.api.DisplayName;
@@ -19,6 +21,9 @@ public class ArticleServiceTest {
 
     @Autowired
     private ArticleService articleService;
+
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @DisplayName(value = "articleCreateDto,userId 유효하면 글 저장 성공")
     @Test
@@ -38,5 +43,48 @@ public class ArticleServiceTest {
 
         assertThat(article.getTitle()).isEqualTo(articleCreateDto.getTitle());
         assertThat(article.getUserId()).isEqualTo(userId);
+    }
+
+    @DisplayName(value = "articleId 유효하면 찾아서 삭제 성공")
+    @Test
+    void t2() throws ArticleNotFoundException {
+
+        creatArticle();
+
+        Long articleId = 1L;
+
+        articleService.delete(articleId);
+
+        assertThat(articleRepository.findById(articleId)).isEmpty();
+    }
+
+    @DisplayName(value = "articleId 유효하지못하면 예외 발생")
+    @Test
+    void t3() {
+
+        creatArticle();
+
+        Long articleId = 10L;
+
+        assertThatThrownBy(() -> {
+
+            articleService.delete(articleId);
+
+        }).isInstanceOf(ArticleNotFoundException.class);
+
+    }
+
+    private void creatArticle() {
+        Article article = Article.builder()
+                .title("안녕하세요")
+                .userId(1L)
+                .career("누구누구 세션 , 교회 반주 10년")
+                .price(30000)
+                .part(Part.LIVE_SESSION)
+                .skill(Skill.GUITAR)
+                .genre(Genre.POP)
+                .build();
+
+        articleRepository.save(article);
     }
 }
