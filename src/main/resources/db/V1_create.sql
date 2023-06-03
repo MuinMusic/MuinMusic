b1cb522070731291a6b9015525d4d2a3c2ece330
sdongpil <pildong94@naver.com>
2023-06-04 06:25:16 +0900

    feat: 기본 설정 db,yml

---
+++ src/main/resources/db/V1_create.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE IF NOT EXISTS `item` (
+   `id` BIGINT NOT NULL AUTO_INCREMENT,
+   `price` INT NOT NULL,
+   `stock` INT NOT NULL,
+   `name` VARCHAR(255) NOT NULL,
+    UNIQUE INDEX `UK_name` (`name`) ,
+    PRIMARY KEY (`id`))
+;
+
+DROP TABLE IF EXISTS `member`;
+CREATE TABLE IF NOT EXISTS  `member` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `address` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`))
+;
+
+DROP TABLE IF EXISTS `order_item`;
+CREATE TABLE IF NOT EXISTS `order_item` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `count` INT NOT NULL,
+    `total_amount` INT NOT NULL,
+    `item_id` BIGINT NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `UK_item_id` (`item_id`) ,
+    CONSTRAINT `FK_order_item_item_id`
+    FOREIGN KEY (`item_id`)
+    REFERENCES `item` (`id`));
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `member_id` BIGINT NOT NULL,
+    `order_date` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
+    `address` VARCHAR(255) NOT NULL,
+    `order_status` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `FK_orders_member_id`
+    FOREIGN KEY (`member_id`)
+    REFERENCES `member` (`id`));
+
+DROP TABLE IF EXISTS `orders_order_items`;
+CREATE TABLE IF NOT EXISTS `orders_order_items` (
+    `order_items_id` BIGINT NOT NULL,
+    `orders_id` BIGINT NOT NULL,
+    UNIQUE INDEX `UK_orders_order_item_order_item_it` (`order_items_id`) ,
+    CONSTRAINT `FK_orders_order_items_orders_id` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`id`),
+    CONSTRAINT `FK_orders_order_items_order_item_id` FOREIGN KEY (`order_items_id`) REFERENCES `order_item` (`id`))
+    ;
