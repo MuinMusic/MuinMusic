f2e299991749ecb88b5aea0af54e1566e64171d7
sdongpil <pildong94@naver.com>
2023-06-05 05:18:00 +0900

    refactor : exception - spring 의존성 제거

--- src/main/java/com/mucompany/muinmusic/exception/ItemNotFoundException.java
+++ src/main/java/com/mucompany/muinmusic/exception/ItemNotFoundException.java
@@ -1,9 +1,5 @@
 package com.mucompany.muinmusic.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ItemNotFoundException extends RuntimeException{
     public ItemNotFoundException() {
         super("상품을 찾을 수 없습니다");
---
+++ src/main/java/com/mucompany/muinmusic/exception/OrderItemNotFoundException.java
@@ -0,0 +1,7 @@
+package com.mucompany.muinmusic.exception;
+
+public class OrderItemNotFoundException extends RuntimeException{
+    public OrderItemNotFoundException() {
+        super("주문 상품을 찾을 수 없습니다");
+    }
+}
