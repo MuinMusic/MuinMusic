10be465f8ee74d3e73d5f7806ae901e1ad389826
sdongpil <pildong94@naver.com>
2023-06-06 07:11:42 +0900

    refactor : db 외래키 제거

--- src/main/resources/db/V1_create.sql
+++ src/main/resources/db/V1_create.sql
@@ -22,11 +22,7 @@ CREATE TABLE IF NOT EXISTS `order_item` (
     `count` INT NOT NULL,
     `total_amount` INT NOT NULL,
     `item_id` BIGINT NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE INDEX `UK_item_id` (`item_id`) ,
-    CONSTRAINT `FK_order_item_item_id`
-    FOREIGN KEY (`item_id`)
-    REFERENCES `item` (`id`));
+    PRIMARY KEY (`id`));
 
 DROP TABLE IF EXISTS `orders`;
 CREATE TABLE IF NOT EXISTS `orders` (
@@ -35,16 +31,9 @@ CREATE TABLE IF NOT EXISTS `orders` (
     `order_date` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
     `address` VARCHAR(50) NOT NULL,
     `order_status` VARCHAR(20) NOT NULL,
-    PRIMARY KEY (`id`),
-    CONSTRAINT `FK_orders_member_id`
-    FOREIGN KEY (`member_id`)
-    REFERENCES `member` (`id`));
+    PRIMARY KEY (`id`));
 
 DROP TABLE IF EXISTS `orders_order_items`;
 CREATE TABLE IF NOT EXISTS `orders_order_items` (
     `order_items_id` BIGINT NOT NULL,
-    `order_id` BIGINT NOT NULL,
-    UNIQUE INDEX `UK_orders_order_item_order_item_it` (`order_items_id`) ,
-    CONSTRAINT `FK_orders_order_items_order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
-    CONSTRAINT `FK_orders_order_items_order_item_id` FOREIGN KEY (`order_items_id`) REFERENCES `order_item` (`id`))
-    ;
+    `order_id` BIGINT NOT NULL );
