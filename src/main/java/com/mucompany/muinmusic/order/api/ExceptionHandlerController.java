e78b13bbbcd4e227b28a30600bf156c08a6ebd9a
sdongpil <pildong94@naver.com>
2023-06-06 07:16:45 +0900

    feat : ExceptionHandlerController 추가

---
+++ src/main/java/com/mucompany/muinmusic/order/api/ExceptionHandlerController.java
@@ -0,0 +1,33 @@
+package com.mucompany.muinmusic.order.api;
+
+import com.mucompany.muinmusic.exception.ItemNotFoundException;
+import com.mucompany.muinmusic.exception.MemberNotFoundException;
+import com.mucompany.muinmusic.exception.OrderItemNotFoundException;
+import com.mucompany.muinmusic.exception.OutOfStockException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlerController {
+    @ExceptionHandler(ItemNotFoundException.class)
+    public ResponseEntity<String> itemNotFoundException(ItemNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<String> memberNotFoundException(MemberNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(OrderItemNotFoundException.class)
+    public ResponseEntity<String> oderItemNotFoundException(OrderItemNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(OutOfStockException.class)
+    public ResponseEntity<String> outOfStockException(OutOfStockException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
