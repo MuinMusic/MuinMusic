67d3aeb161ea8b9f6848787a10fff9134dc767e2
sdongpil <pildong94@naver.com>
2023-06-06 07:22:15 +0900

    test : 예외 발생 시 실패하는 테스트 추가

--- src/test/java/com/mucompany/muinmusic/order/api/OrderControllerTest.java
+++ src/test/java/com/mucompany/muinmusic/order/api/OrderControllerTest.java
@@ -3,18 +3,21 @@ package com.mucompany.muinmusic.order.api;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mucompany.muinmusic.Item.domain.Item;
 import com.mucompany.muinmusic.Item.repository.ItemRepository;
+import com.mucompany.muinmusic.exception.ItemNotFoundException;
+import com.mucompany.muinmusic.exception.MemberNotFoundException;
+import com.mucompany.muinmusic.exception.OrderItemNotFoundException;
+import com.mucompany.muinmusic.exception.OutOfStockException;
 import com.mucompany.muinmusic.member.domain.Member;
-import com.mucompany.muinmusic.member.repository.MemberRepository;
+import com.mucompany.muinmusic.member.domain.repository.MemberRepository;
 import com.mucompany.muinmusic.order.domain.OrderItem;
 import com.mucompany.muinmusic.order.domain.OrderStatus;
-import com.mucompany.muinmusic.order.repository.OrderItemRepository;
+import com.mucompany.muinmusic.order.domain.repository.OrderItemRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +25,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -29,7 +34,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-@Sql(value = "classpath:db/resetPK.sql")
 public class OrderControllerTest {
 
     @Autowired
@@ -50,7 +54,6 @@ public class OrderControllerTest {
 
         String requestBody = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(orderRequestDto);
 
-
         mockMvc.perform(post("/api/orders").contentType(MediaType.APPLICATION_JSON).content(requestBody))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$..memberId").value(1))
@@ -60,6 +63,97 @@ public class OrderControllerTest {
                 .andExpect(jsonPath("$..orderDate").exists());
     }
 
+    @DisplayName(value = "orderRequestDto.memberId로 member 찾지 못할 경우 MemberNotFoundException 발생 및 HTTP404 응답")
+    @Test
+    void t2() throws Exception {
+        OrderRequestDto orderRequestDto = createOrderRequestDto();
+
+        orderRequestDto.setMemberId(100L);
+
+        String requestBody = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(orderRequestDto);
+
+        mockMvc.perform(post("/api/orders").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+                .andExpect(status().isNotFound())
+                .andExpect(result -> {
+                    Throwable exception = result.getResolvedException();
+                    assertNotNull(exception);
+                    assertEquals(MemberNotFoundException.class, exception.getClass());
+                    assertEquals("회원을 찾을 수 없습니다", exception.getMessage());
+                });
+    }
+
+    @DisplayName(value = "orderRequestDto.orderItemIdList 로 orderItem 찾지 못할 경우 OrderItemNotFoundException 발생 및 HTTP404 응답")
+    @Test
+    void t3() throws Exception {
+        OrderRequestDto orderRequestDto = createOrderRequestDto();
+
+        List<Long> orderItemIdList = List.of(100L, 200L);
+
+        orderRequestDto.setOrderItemIdList(orderItemIdList);
+
+        String requestBody = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(orderRequestDto);
+
+        mockMvc.perform(post("/api/orders").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+                .andExpect(status().isNotFound())
+                .andExpect(result -> {
+                    Throwable exception = result.getResolvedException();
+                    assertNotNull(exception);
+                    assertEquals(OrderItemNotFoundException.class, exception.getClass());
+                    assertEquals("주문 상품을 찾을 수 없습니다", exception.getMessage());
+                });
+    }
+
+    @DisplayName(value = "주문하려는 Item 상품 없을 시 ItemNotFoundException 발생 및 HTTP404 응답")
+    @Test
+    void t4() throws Exception {
+        OrderRequestDto orderRequestDto = createOrderRequestDto();
+
+        List<Long> orderItemIdList = orderRequestDto.getOrderItemIdList();
+        List<Item> item = new ArrayList<>();
+        for (Long orderItemId : orderItemIdList) {
+            OrderItem result = orderItemRepository.findById(orderItemId).orElseThrow(OrderItemNotFoundException::new);
+            item.add(result.getItem());
+        }
+        itemRepository.delete(item.get(1));
+
+        String requestBody = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(orderRequestDto);
+
+        mockMvc.perform(post("/api/orders").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+                .andExpect(status().isNotFound())
+                .andExpect(result -> {
+                    Throwable exception = result.getResolvedException();
+                    assertNotNull(exception);
+                    assertEquals(ItemNotFoundException.class, exception.getClass());
+                    assertEquals("상품을 찾을 수 없습니다", exception.getMessage());
+                });
+    }
+
+    @DisplayName(value = "주문하려는 Item의 재고수량 초과했을경우 OutOfStockException 발생 및 HTTP404 응답")
+    @Test
+    void t5() throws Exception {
+        OrderRequestDto orderRequestDto = createOrderRequestDto();
+        //아이템의 수량은 5개로 셋팅해놨다
+        Item book = new Item("개구리책", 20000, 5);
+        itemRepository.save(book);
+        //6개를 주문한다면
+        OrderItem orderItem = new OrderItem(book, 6, 120000);
+        orderItemRepository.save(orderItem);
+
+        List<Long> orderItemIdList = List.of(orderItem.getId());
+        orderRequestDto.setOrderItemIdList(orderItemIdList);
+
+        String requestBody = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(orderRequestDto);
+
+        mockMvc.perform(post("/api/orders").contentType(MediaType.APPLICATION_JSON).content(requestBody))
+                .andExpect(status().isNotFound())
+                .andExpect(result -> {
+                    Throwable exception = result.getResolvedException();
+                    assertNotNull(exception);
+                    assertEquals(OutOfStockException.class, exception.getClass());
+                    assertEquals("상품의 재고가 부족합니다", exception.getMessage());
+                });
+    }
+
     private OrderRequestDto createOrderRequestDto() {
         Member member = new Member("dp", "seoul");
         Item item = new Item("jpaBook", 20000, 10);
@@ -67,16 +161,13 @@ public class OrderControllerTest {
         OrderItem orderItem = new OrderItem(item, 3, 60000);
         OrderItem orderItem2 = new OrderItem(item2, 1, 20000);
 
-        List<Long> orderItemIdList = new ArrayList<>();
-
         Member saveMember = memberRepository.save(member);
         itemRepository.save(item);
         itemRepository.save(item2);
         orderItemRepository.save(orderItem);
         orderItemRepository.save(orderItem2);
 
-        orderItemIdList.add(orderItem.getId());
-        orderItemIdList.add(orderItem2.getId());
+        List<Long> orderItemIdList = List.of(orderItem.getId(), orderItem2.getId());
 
         return OrderRequestDto.builder()
                 .memberId(saveMember.getId())
