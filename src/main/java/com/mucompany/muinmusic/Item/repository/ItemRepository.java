d28751d1767d5ce840de9c2405573525560e8e84
sdongpil <pildong94@naver.com>
2023-06-19 20:30:40 +0900

    [I-15] refactor: PessimisticLock 적용

--- src/main/java/com/mucompany/muinmusic/Item/repository/ItemRepository.java
+++ src/main/java/com/mucompany/muinmusic/Item/repository/ItemRepository.java
@@ -1,9 +1,16 @@
 package com.mucompany.muinmusic.Item.repository;
 
 import com.mucompany.muinmusic.Item.domain.Item;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("select i from Item i where i.id =:id")
+    Item findByIdWithPessimisticLock(Long id) ;
 }
