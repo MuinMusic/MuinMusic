86a246b5918f2a1242345054a7592e619a13f618
sdongpil <pildong94@naver.com>
2023-07-03 23:26:43 +0900

    [I-15] refactor: redissonLock -> pessimisticLock 으로 변경

---
+++ src/main/java/com/mucompany/muinmusic/item/app/ItemService.java
@@ -0,0 +1,26 @@
+package com.mucompany.muinmusic.item.app;
+
+import com.mucompany.muinmusic.cart.domain.CartItem;
+import com.mucompany.muinmusic.exception.ItemNotFoundException;
+import com.mucompany.muinmusic.item.domain.Item;
+import com.mucompany.muinmusic.item.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void itemStockDecrease(CartItem cartItem, Long itemId) {
+        Item item = itemRepository.findById(itemId).orElseThrow(ItemNotFoundException::new);
+        item.decrease(cartItem.getCount());
+        log.info("item.getStock() = " + item.getStock());
+        itemRepository.save(item);
+    }
+}
