76744b6421e7e3bf0470d7352c4098af0c7895c7
sdongpil <pildong94@naver.com>
2023-08-16 01:25:54 +0900

    [I-15] feat: OrderController - 주문 취소내역 가져오기

--- src/main/java/com/mucompany/muinmusic/order/api/OrderController.java
+++ src/main/java/com/mucompany/muinmusic/order/api/OrderController.java
@@ -62,6 +62,13 @@ public class OrderController {
         return ResponseEntity.status(HttpStatus.OK).body(orderHistory);
     }
 
+    @GetMapping(value = "/orders/cancel")
+    public ResponseEntity<List<OrderDto>> getCancelOrderHistory(@RequestParam Long memberId,@PageableDefault(page = 0,size = 10,sort = "id",direction = Sort.Direction.DESC) Pageable pageable) {
+        List<OrderDto> orderHistory = orderService.getCancelOrderHistory(memberId,pageable);
+
+        return ResponseEntity.status(HttpStatus.OK).body(orderHistory);
+    }
+
     private static OrderRequest createOrderRequest(OrderRequestDto orderRequestDto) {
         return OrderRequest.builder()
                 .memberId(orderRequestDto.getMemberId())
