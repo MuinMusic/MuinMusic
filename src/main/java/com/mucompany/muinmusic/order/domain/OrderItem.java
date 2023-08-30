819f1f220b6916f0e8c09d0fbc796bba1870aafe
sdongpil <pildong94@naver.com>
2023-08-31 02:16:04 +0900

    [I-15] feat: orderItem - 주문상태 추가

--- src/main/java/com/mucompany/muinmusic/order/domain/OrderItem.java
+++ src/main/java/com/mucompany/muinmusic/order/domain/OrderItem.java
@@ -1,8 +1,11 @@
 package com.mucompany.muinmusic.order.domain;
 
 import com.mucompany.muinmusic.cart.domain.CartItem;
+import com.mucompany.muinmusic.exception.OrderCancellationException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,11 +32,22 @@ public class OrderItem {
     @Column(nullable = false)
     private int totalAmount;
 
+    @Column
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
     public OrderItem(CartItem cartItem) {
         Assert.notNull(cartItem, "cartItem can not be null");
 
         this.itemId = cartItem.getItemId();
         this.count = cartItem.getCount();
         this.totalAmount = cartItem.getTotalAmount();
+    }
+
+    public void cancel() {
+        if (orderStatus.equals(OrderStatus.SHIPPING)) {
+            throw new OrderCancellationException();
+        }
+        this.orderStatus = OrderStatus.CANCELLED;
     }
 }
